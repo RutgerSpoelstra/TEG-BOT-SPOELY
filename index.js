b0c7819718a878801b22c23cc50539fdf9a6d48f
RutgerSpoelstra <39886106+RutgerSpoelstra@users.noreply.github.com>
2018-06-04 18:38:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  bot.user.setGame('Bot is online');
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find('name', 'member-log');
+    if (!channel) return;
+    channel.send(`Welkom, ${member}, op de server!`);
+  });
+
+client.login();
